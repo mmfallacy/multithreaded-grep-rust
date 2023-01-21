f8f2c957f8df5df6ad7737927ab9b056479d164e
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-01-22 05:49:42 +0800

    first attempt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,89 @@
-use std::process::{Command, Output};
+#![allow(unused)]
+use core::panic;
+use std::{
+    collections::VecDeque,
+    fs,
+    process::{Command, Output},
+    sync::Arc,
+    sync::Mutex,
+    thread,
+};
 
-fn grep() -> Output {
-    Command::new("ls")
+fn grep(path: &str, search_string: &str) -> Output {
+    Command::new("grep")
+        .arg(search_string)
+        .arg(path)
         .output()
         .expect(" Failed to execute grep")
 }
-
+struct TaskQueue {
+    q: VecDeque<String>,
+    rootnest: i32,
+}
 fn main() {
-    println!("Hello, world!");
+    let n = 2;
+    let rootpath = "E:\\Projects\\multithreaded-grep-rust\\test";
+    let search_string = "Handwire";
 
-    let res = grep();
-    println!(
-        "Success: {:?}\nOutput:\n{}",
-        res.status,
-        String::from_utf8_lossy(&res.stdout)
-    );
+    let tq = Arc::new(Mutex::new(TaskQueue {
+        q: VecDeque::new(),
+        rootnest: 0,
+    }));
+
+    {
+        let mut tq = tq.lock().unwrap();
+        tq.q.push_back(String::from(rootpath));
+        tq.rootnest += 1;
+    }
+
+    for wid in 0..n {
+        let tq = tq.clone();
+        let worker = thread::spawn(move || loop {
+            // Test
+            let path: String;
+            {
+                let mut tq = tq.lock().unwrap();
+
+                if tq.rootnest == 0 {
+                    break;
+                };
+
+                if let Some(elem) = tq.q.pop_front() {
+                    path = elem;
+                } else {
+                    continue;
+                }
+            }
+
+            if let Ok(dir) = fs::read_dir(path) {
+                for entry in dir {
+                    let entry = entry.unwrap();
+                    let metadata = fs::metadata(entry.path()).unwrap();
+
+                    if metadata.is_dir() {
+                        let mut tq = tq.lock().unwrap();
+                        tq.q.push_back(entry.path().to_str().unwrap().to_owned());
+                        tq.rootnest += 1;
+                        println!("[{}] DIR {:?}", wid, entry.path());
+                    } else if metadata.is_file() {
+                        let status = grep(entry.path().to_str().unwrap(), search_string)
+                            .status
+                            .success();
+                        println!(
+                            "[{}] {} {:?}",
+                            wid,
+                            if status { "PRESENT" } else { "ABSENT" },
+                            entry.path()
+                        );
+                    }
+                }
+            };
+
+            {
+                let mut tq = tq.lock().unwrap();
+                tq.rootnest -= 1;
+            }
+        });
+        worker.join().unwrap();
+    }
 }
